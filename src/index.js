22e61dc2898969e52aa502482fc2e17687c95389
Abhinav3499 <3499arya@gmail.com>
2026-01-19 16:35:38 +0530

    router setup completed with post request

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ const bodyParser = require('body-parser')
 
 const { PORT } = require('./config/serverConfig')
 // const CityRepo = require('./repository/city-repository')
+const APIRoutes = require('./routes/index');
 
 const setupAndStartServer = async() => {
     const app = express(); 
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api', APIRoutes);
 
     app.listen(PORT, () => {
         console.log(`server started on port ${PORT}`);
